758e8920d9f707c2aaabe0224c4e50fb354ba591
luiofff <95875204+luiofff@users.noreply.github.com>
2022-10-18 23:34:01 +0300

    addRelocation

---
+++ js/index.js
@@ -0,0 +1,12 @@
+aboutUsJs.addEventListener("click", () => {
+    window.location.href = "/second-page/index.html";
+})
+adressJs.addEventListener("click", () => {
+    window.location.href = "/third-page/index.html";
+})
+contactsJs.addEventListener("click", () => {
+    window.location.href = "/four-page/index.html";
+})
+logoJs.addEventListener("click", () => {
+    window.location.href = "/first-page/index.html";
+})
